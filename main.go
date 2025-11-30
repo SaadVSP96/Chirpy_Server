3aa093b9dd963bc5e17f07a3e625f4d23c6a05eb
root <root@DESKTOP-TVOVS74.localdomain>
2025-11-30 15:37:35 +0500

    basic file sending on root path using builting functions as handlers is now working

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	mux := http.NewServeMux() // router
-
+	mux.Handle("/", http.FileServer(http.Dir(".")))
 	server := &http.Server{
 		Addr:    ":8080", // Bind to localhost:8080
 		Handler: mux,     // Use our empty mux
