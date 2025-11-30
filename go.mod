b0c764bb67c7dd626112c95ee2385ba165fbf699
SaadVSP96 <saadmasood192@gmail.com>
2025-11-30 15:04:37 +0500

    project initialized

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/SaadVSP96/Chirpy_Server.git
+
+go 1.25.4
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	mux := http.NewServeMux() // router
+
+	server := &http.Server{
+		Addr:    ":8080", // Bind to localhost:8080
+		Handler: mux,     // Use our empty mux
+	}
+
+	log.Println("server running on :8080")
+	log.Fatal(server.ListenAndServe())
+
+}
